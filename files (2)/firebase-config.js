c1af792c41aeb3359a8847b373851e9c255e7b77
nawriscontroltower <medodaw519@gmail.com>
2026-06-06 02:04:38 +0200

    Add files via upload

---
+++ files (2)/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjUNyQyDtInmf9Qek7twWNAK6qzVyjI4Y",
+  authDomain: "firestore-3cfc8.firebaseapp.com",
+  projectId: "firestore-3cfc8",
+  storageBucket: "firestore-3cfc8.firebasestorage.app",
+  messagingSenderId: "24097784465",
+  appId: "1:24097784465:web:ba7216862b1807b9993530"
+};
